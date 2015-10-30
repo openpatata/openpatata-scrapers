e77d24f46608d55502231836487400877f25fbc1
wfdd <erdbanfed@gmail.com>
2015-10-30 23:36:43 +0200

    Pull in all scraper modules for doctesting

--- test_scrapers.py
+++ test_scrapers.py
@@ -1,12 +1,14 @@
 
 import doctest
 from functools import reduce
+from importlib.machinery import SourceFileLoader
+from pathlib import Path
 import sys
-
-from scrapers import records, text_utils
 
 if __name__ == '__main__':
     failed = reduce(lambda c, m: c+doctest.testmod(m, verbose=True).failed,
-                    (records, text_utils), 0)
+                    map(lambda m: SourceFileLoader(*(str(m),)*2).load_module(),
+                        Path('./scrapers').glob('*.py')), 0)
     print('Total failures:', failed)
+
     sys.exit(int(bool(failed)))
