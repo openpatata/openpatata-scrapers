dae04bf8d6348bb9c84179e24140c72a36f602bf
wfdd <erdbanfed@gmail.com>
2015-10-30 23:35:56 +0200

    Turns out subclassing Loader and Dumper is not such a good idea

--- scrapers/io.py
+++ scrapers/io.py
@@ -5,51 +5,72 @@ from collections import OrderedDict
 import os
 
 import yaml
-from yaml.resolver import BaseResolver
 
 
-class _YamlLoader(yaml.CLoader):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.add_constructor(BaseResolver.DEFAULT_MAPPING_TAG,
-                             self.construct_mapping)
-
-    def construct_mapping(self, _, node, deep=False):
-        """Maintain the order of !maps on import."""
-        return OrderedDict((self.construct_object(k, deep=deep),
-                            self.construct_object(v, deep=deep))
-                           for k, v in node.value)
+class DumpError(Exception):
+    """Exception raised by `YamlManager`."""
 
 
-class _YamlDumper(yaml.CDumper):
+class _YamlConstructor(yaml.constructor.SafeConstructor):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.add_representer(str, self.represent_scalar)
+    def construct_mapping(self, node, deep=False):
+        """Alpha-sort !maps on import."""
+        val = super().construct_mapping(node, deep)
+        return OrderedDict(sorted(val.items()))
 
-    def represent_scalar(self, _, value, style=None):
+_YamlConstructor.add_constructor(yaml.resolver.Resolver.DEFAULT_MAPPING_TAG,
+                                 _YamlConstructor.construct_mapping)
+
+
+class _YamlLoader(yaml.CSafeLoader, _YamlConstructor):
+    """Inject our `_YamlConstructor` before `yaml.CSafeLoader`'s."""
+
+
+class _YamlRepresenter(yaml.representer.SafeRepresenter):
+
+    def represent_str(self, data):
         """Apply block style to multi-line strings."""
-        style = '|' if '\n' in value else None
-        return super().represent_scalar(BaseResolver.DEFAULT_SCALAR_TAG,
-                                        value, style)
+        style = '|' if '\n' in data else None
+        return self.represent_scalar(yaml.resolver.Resolver.DEFAULT_SCALAR_TAG,
+                                     data, style)
+
+_YamlRepresenter.add_representer(str,
+                                 _YamlRepresenter.represent_str)
+_YamlRepresenter.add_representer(OrderedDict,
+                                 # PyYAML sorts dicts on export out of the box
+                                 _YamlRepresenter.represent_dict)
+
+
+class _YamlDumper(yaml.CSafeDumper, _YamlRepresenter):
+    """Inject our `_YamlRepresenter` before `CSafeDumper`'s."""
 
 
 class YamlManager:
 
     @staticmethod
-    def load(path, filename):
+    def load(path):
         """Import a document from disk."""
         with open(path) as file:
             doc = yaml.load(file.read(),
                             Loader=_YamlLoader)
-            doc['_filename'] = filename
             return doc
 
+    @classmethod
+    def load_record(cls, path, filename):
+        """Import a database record from disk."""
+        doc = cls.load(path)
+        doc['_filename'] = filename
+        return doc
+
     @staticmethod
-    def dump(doc, head):
-        """Save a document to disk."""
-        path = os.path.join(head, doc.pop('_filename'))
+    def dump_record(doc, head):
+        """Save a database record to disk."""
+        try:
+            path = os.path.join(head, doc.pop('_filename'))
+            del doc['_id']
+        except KeyError:
+            raise DumpError("Unable to extract filename from record with"
+                            " `_id` '{}'".format(doc['_id'])) from None
         with open(path, 'w') as file:
             yaml.dump(doc, file,
                       Dumper=_YamlDumper,
