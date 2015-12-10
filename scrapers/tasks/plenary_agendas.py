3fb81a3704bea21bb07920016b61e94853611f38
wfdd <erdbanfed@gmail.com>
2015-12-10 23:23:18 +0200

    More robust stuffs

--- scrapers/tasks/plenary_agendas.py
+++ scrapers/tasks/plenary_agendas.py
@@ -25,8 +25,13 @@ class PlenaryAgendas(Task):
             {self.process_agenda_listing(href)
              for href in html.xpath('//a[@class="h3Style"]/@href')})
         agenda_urls = itertools.chain.from_iterable(agenda_urls)
-        await self.crawler.gather({self.process_agenda(href)
-                                   for href in agenda_urls})
+
+        bills = await self.crawler.gather({self.process_agenda(href)
+                                           for href in agenda_urls})
+        bills = itertools.chain.from_iterable(bills)
+        await self.crawler.gather(
+            {self.crawler.exec_blocking(parse_agenda_bill, *bill)
+             for bill in bills})
 
     __call__ = process_agenda_index
 
@@ -51,16 +56,17 @@ class PlenaryAgendas(Task):
 
     async def process_agenda(self, url):
         if url[-4:] == '.pdf':
-            text = await self.crawler.get_payload(url)
-            text = await self.crawler.exec_blocking(pdf2text, text)
-            await self.crawler.exec_blocking(parse_pdf_agenda, url, text)
+            payload = await self.crawler.get_payload(url)
+            text = await self.crawler.exec_blocking(pdf2text, payload)
+            return await self.crawler.exec_blocking(parse_pdf_agenda,
+                                                    url, text)
         else:
             html = await self.crawler.get_html(url)
-            await self.crawler.exec_blocking(parse_agenda, url, html)
+            return await self.crawler.exec_blocking(parse_agenda, url, html)
 
 
-RE_PP = re.compile(r'(\w+)[\'΄] ΒΟΥΛΕΥΤΙΚΗ ΠΕΡΙΟΔΟΣ')
-RE_SE = re.compile(r'ΣΥΝΟΔΟΣ (\w+)[\'΄]')
+RE_PP = re.compile(r'(\w+)[\'΄] ΒΟΥΛΕΥΤΙΚΗ ΠΕΡΙΟ[Δ∆]ΟΣ')
+RE_SE = re.compile(r'ΣΥΝΟ[Δ∆]ΟΣ (\w+)[\'΄]')
 RE_SI = re.compile(r'(\d+)[ηή] ?συνεδρίαση')
 RE_ID = re.compile(r'([12]3\.[0-9.-]+)')
 
@@ -71,19 +77,39 @@ def _extract_parliamentary_period(url, text):
         'Unable to extract parliamentary period of {!r}'.format(url))
 
 
-def _parse_session(url, text):
+def _extract_session(url, text):
     match = RE_SE.search(text)
     return match.group(1) if match else logger.warning(
         'Unable to extract session of {!r}'.format(url))
 
 
-def _parse_sitting(url, text):
+def _extract_sitting(url, text):
     match = RE_SI.search(text)
     return int(match.group(1)) if match else logger.warning(
         'Unable to extract sitting number of {!r}'.format(url))
 
 
-def _parse_agenda_items(url, html):
+def _arrange_agenda_items(url, items):
+    # 23.01        government bill
+    # 23.02        member's bill
+    # 23.03        draft regulations
+    # 23.04        something or other to do with committees
+    # 23.05        debate topic
+    # 23.10.100    draft resolution
+    # 23.10.200    decision
+    # 23.15        house rules of procedure
+    bills = odict((k, v) for k, v in items.items()
+                  if k.startswith(('23.01', '23.02', '23.03')))
+    debate_topics = odict((k, v) for k, v in items.items()
+                          if k.startswith('23.05'))
+    unparsed = items.keys() - bills.keys() - debate_topics.keys()
+    if unparsed:
+        logger.warning('Unparsed items {} in {!r}'.format(unparsed, url))
+    return bills, debate_topics
+
+
+def parse_agenda(url, html):
+    agenda_items = []
     for e in html.xpath('//div[@class="articleBox"]//tr/td[last()]'):
         try:
             title, *ex, id_ = (clean_spaces(e.text_content())
@@ -94,24 +120,13 @@ def _parse_agenda_items(url, html):
 
         id_ = RE_ID.search(''.join(ex + [id_]))
         if id_:
-            yield id_.group(1), title.rstrip('.')
+            agenda_items.append((id_.group(1), title.rstrip('.')))
         else:
             logger.warning('Unable to extract document type'
                            ' of {!r} in {!r}'.format(title, url))
 
-
-def parse_agenda(url, html):
-    all_items = odict(_parse_agenda_items(url, html))
-    bills = odict((k, v) for k, v in all_items.items()
-                  if k.startswith(('23.01', '23.02', '23.03')))
-    debate_topics = odict((k, v) for k, v in all_items.items()
-                          if k.startswith('23.05'))
-
-    unparsed = all_items.keys() - bills.keys() - debate_topics.keys()
-    if unparsed:
-        logger.warning('Unparsed items {} in {!r}'.format(unparsed, url))
-
-    body_text = clean_spaces(html.xpath('string(//div[@class="articleBox"])'))
+    text = clean_spaces(html.xpath('string(//div[@class="articleBox"])'))
+    bills, debate_topics = _arrange_agenda_items(url, odict(agenda_items))
 
     plenary_sitting = PlenarySitting.from_template(
         None,
@@ -120,18 +135,17 @@ def parse_agenda(url, html):
          'date': parse_long_date(clean_spaces(html.xpath('string(//h1)')),
                                  plenary=True),
          'links': [{'type': 'agenda', 'url': url}],
-         'parliamentary_period': _extract_parliamentary_period(url, body_text),
-         'session': _parse_session(url, body_text),
-         'sitting': _parse_sitting(url, body_text)})
+         'parliamentary_period': _extract_parliamentary_period(url, text),
+         'session': _extract_session(url, text),
+         'sitting': _extract_sitting(url, text)})
     if not plenary_sitting.insert():
         logger.warning('Unable to insert or update plenary on {!r}'
                        ' in {!r}'.format(plenary_sitting['date'], url))
 
-    for uid, title in bills.items():
-        parse_agenda_bill(url, uid, title)
+    return zip(itertools.repeat(url), bills.keys(), bills.values())
 
 
-RE_PAGENO = re.compile(r'^ +\d+\n')
+RE_PAGENO = re.compile(r'^ +(?:\d+|\w)\n')
 RE_TITLE_OTHER = re.compile(r'\(Πρόταση νόμου[^\)]*\)')
 
 
@@ -144,45 +158,41 @@ def parse_pdf_agenda(url, text):
             _store[0] = key[0]
             return _store[0]
 
-    def _prepare_item(key, value):
-        id_ = RE_ID.search(key)
-        if not id_:
-            return logger.warning('Unable to extract document type of {}'
-                                  ' in {!r}'.format((key, value), url))
-        return (id_.group(1),
-                RE_TITLE_OTHER.sub('', ' '.join(value)).rstrip('. '))
+    def _prepare_agenda_item(item_):
+        if not any(item_):
+            return
 
-    if (url == 'http://www.parliament.cy/images/media/redirectfile/13-0312015-'
-               ' agenda ΤΟΠΟΘΕΤΗΣΕΙΣ doc.pdf'):
+        item = itertools.dropwhile(lambda v: not RE_ID.search(v),
+                                   reversed(item_))
+        item = tuple(reversed(tuple(item)))   # ...
+        if not item:
+            logger.warning('Unable to extract document type of {}'
+                           ' in {!r}'.format(item_, url))
+            return
+
+        id_, title = item[-1], item[:-1]
+        return (RE_ID.search(id_).group(1),
+                RE_TITLE_OTHER.sub('', ' '.join(title)).rstrip('. '))
+
+    if (url == 'http://www.parliament.cy/images/media/redirectfile/'
+               '13-0312015- agenda ΤΟΠΟΘΕΤΗΣΕΙΣ doc.pdf'):
         # `TableParser` chokes on its mixed two- and three-col layout
         logger.warning('Skipping {!r} in `parse_pdf_agenda`'.format(url))
-        return
+        return ()
 
     pages = text.split('\x0c')
-    rows = itertools.chain.from_iterable(
-        # Get rid of the page numbers first
-        TableParser(RE_PAGENO.sub('', page), 2).rows for page in pages)
-
-    # Parse the pages into an array of agenda items, grouped by their list
-    # number, each containing an array of table rows
-    agenda_items = (tuple(x[-1] for x in v)
+    # Get rid of the page numbers 'cause they might intersect items in the list
+    pages_ = (RE_PAGENO.sub('', page) for page in pages)
+    rows = itertools.chain.from_iterable(TableParser(page, 2).rows
+                                         for page in pages_)
+    # Group rows into tuples, using the leftmost cell as a key, which oughta
+    # either contain a list number or be left blank
+    agenda_items = (tuple(zip(*v))[-1]
                     for _, v in itertools.groupby(rows, key=_group_by_key))
-    # Pop from the end of the list to create a two-tuple; the assumption's
-    # that the bottom line contains the item's id
-    agenda_items = ((v[-1], v[:-1]) for v in agenda_items)
-    # Concatenate the rows into a string, extract the id from the key,
-    # and throw out the junk
-    agenda_items = filter(None, itertools.starmap(_prepare_item, agenda_items))
-    agenda_items = odict(agenda_items)
-
-    bills = odict((k, v) for k, v in agenda_items.items()
-                  if k.startswith(('23.01', '23.02', '23.03')))
-    debate_topics = odict((k, v) for k, v in agenda_items.items()
-                          if k.startswith('23.05'))
-
-    unparsed = agenda_items.keys() - bills.keys() - debate_topics.keys()
-    if unparsed:
-        logger.warning('Unparsed items {} in {!r}'.format(unparsed, url))
+    # Concatenate the rows into a title string, extract the id, and throw out
+    # the junk; the output's an array of id–title two-tuples
+    agenda_items = filter(None, map(_prepare_agenda_item, agenda_items))
+    bills, debate_topics = _arrange_agenda_items(url, odict(agenda_items))
 
     plenary_sitting = PlenarySitting.from_template(
         None,
@@ -192,14 +202,13 @@ def parse_pdf_agenda(url, text):
                                  plenary=True),
          'links': [{'type': 'agenda', 'url': url}],
          'parliamentary_period': _extract_parliamentary_period(url, text),
-         'session': _parse_session(url, text),
-         'sitting': _parse_sitting(url, text)})
+         'session': _extract_session(url, text),
+         'sitting': _extract_sitting(url, text)})
     if not plenary_sitting.insert():
         logger.warning('Unable to insert or update plenary on {!r}'
                        ' in {!r}'.format(plenary_sitting['date'], url))
 
-    for uid, title in bills.items():
-        parse_agenda_bill(url, uid, title)
+    return zip(itertools.repeat(url), bills.keys(), bills.values())
 
 
 def parse_agenda_bill(url, uid, title):
